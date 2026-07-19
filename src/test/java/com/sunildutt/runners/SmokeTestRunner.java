da804a49a842e04452797e229a839012e280f7f9
Sunil Dutt <sunil.dutt28@gmail.com>
2026-07-19 17:59:43 +0100

    TestActivityPredictionUIAutomation

---
+++ src/test/java/com/sunildutt/runners/SmokeTestRunner.java
@@ -0,0 +1,23 @@
+package com.sunildutt.runners;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(
+        key = GLUE_PROPERTY_NAME,
+        value = "com.sunildutt.stepdefinitions")
+@ConfigurationParameter(
+        key = PLUGIN_PROPERTY_NAME,
+        value = "pretty,html:target/cucumber-smoke-report.html")
+@ConfigurationParameter(
+        key = FILTER_TAGS_PROPERTY_NAME,
+        value = "@smoke")
+public class SmokeTestRunner {
+}
