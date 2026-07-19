5c30db17ad06736ba2e901e6f22e3e55a9e17457
Sunil Dutt <sunil.dutt28@gmail.com>
2026-07-19 16:23:11 +0100

    TestActivityPredictionUIAutomation

--- src/test/java/com/sunildutt/runners/TestRunner.java
+++ src/test/java/com/sunildutt/runners/TestRunner.java
@@ -1,7 +1,21 @@
 package com.sunildutt.runners;
 
-import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@Cucumber
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(
+        key = GLUE_PROPERTY_NAME,
+        value = "com.sunildutt.stepdefinitions")
+@ConfigurationParameter(
+        key = PLUGIN_PROPERTY_NAME,
+        value = "pretty, html:target/cucumber-report.html")
 public class TestRunner {
 }
