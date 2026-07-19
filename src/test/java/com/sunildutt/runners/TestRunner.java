ddf30000dc95fcf3c273a8ce89ede1f5c2bfac50
Sunil Dutt <sunil.dutt28@gmail.com>
2026-07-19 17:33:11 +0100

    TestActivityPredictionUIAutomation

--- src/test/java/com/sunildutt/runners/TestRunner.java
+++ src/test/java/com/sunildutt/runners/TestRunner.java
@@ -4,6 +4,7 @@ import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
+import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
@@ -17,5 +18,8 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @ConfigurationParameter(
         key = PLUGIN_PROPERTY_NAME,
         value = "pretty,html:target/cucumber-report.html")
+@ConfigurationParameter(
+        key = FILTER_TAGS_PROPERTY_NAME,
+        value = "@smoke")
 public class TestRunner {
 }
